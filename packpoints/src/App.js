e6c1016a27b245f7f6108be8d8a6d3340f31e7de
Brock <brockbvp@gmail.com>
2023-01-29 18:16:24 -0800

    added some last little bits of functionality

--- packpoints/src/App.js
+++ packpoints/src/App.js
@@ -1,4 +1,4 @@
-import { dataProcessing } from './database';
+import { dataProcessing, sortByHall } from './database';
 import React, { useState, useEffect } from 'react';
 import Select from 'react-select'
 
@@ -14,23 +14,25 @@ const App = () => {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [users, setUsers] = useState([]);
+  const [allUsers, setAllUsers] = useState([]);
   const [hall, setHall] = useState("");
 
   const options = [
-    { value: 'Argenta', label: 'Argenta' },
-    { value: 'Canada', label: 'Canada' },
-    { value: 'Great Basin', label: 'Great Basin' },
-    { value: 'Man/Jun', label: 'Man/Jun' },
+    { value: 'Argenta Hall', label: 'Argenta' },
+    { value: 'Canada Hall', label: 'Canada' },
+    { value: 'Great Basin Hall', label: 'Great Basin' },
+    { value: 'Manzanita/Juniper Hall', label: 'Manzanita/Juniper' },
     { value: 'Nevada LLC', label: 'Nevada LLC' },
-    { value: 'Nye', label: 'Nye' },
-    { value: 'Peavine', label: 'Peavine' },
-    { value: 'Sierra', label: 'Sierra' },
+    { value: 'Nye Hall', label: 'Nye' },
+    { value: 'Peavine Hall', label: 'Peavine' },
+    { value: 'Sierra Hall', label: 'Sierra' },
     { value: 'All Halls', label: 'All Halls' }
   ]
 
   const handleHallChange = (selectedOption) => {
     setHall(selectedOption);
     console.log(`Option selected:`, selectedOption);
+    setUsers(sortByHall(allUsers, selectedOption));
   }
   
   useEffect(() => {
@@ -44,6 +46,7 @@ const App = () => {
         (result) => {
           setIsLoaded(true);
           setUsers(dataProcessing(result));
+          setAllUsers(dataProcessing(result));
         },
         // Note: it's important to handle errors here
         // instead of a catch() block so that we don't swallow
@@ -53,7 +56,7 @@ const App = () => {
           setError(error);
         }
       )
-  }, [hall])
+  }, [])
 
   if (error) {
     return <div>Error: {error.message}</div>;
--- packpoints/src/database.js
+++ packpoints/src/database.js
@@ -29,7 +29,22 @@ const emails = new Set()
 
 const accounts = []
 
+const compare = (a, b) => {
+    if ( a.points < b.points ){
+        return 1;
+    }
+    if ( a.points > b.points ){
+        return -1;
+    }
+    return 0;
+}
+
 export const dataProcessing = (result) => {
+
+    for (const account of accounts) {
+        account['points'] = 0;
+    }
+
     result = result['GoogleSheetData']
     for (let i = 0; i < result.length; i++) {
         let newResult = result[i]
@@ -47,8 +62,21 @@ export const dataProcessing = (result) => {
             }
         }
     }
-    console.log(accounts)
-    console.log(emails)
-    return accounts;
+    return accounts.sort( compare );
+}
+
+export const sortByHall = (data, hall) => {
+    console.log('Hello?')
+    hall = hall['value']
+    console.log(hall)
+    console.log(data);
+    let output = []
+    for (let i = 0; i < data.length; i++) {
+        if (data[i]['hall'] === hall || hall === 'All Halls') {
+            console.log('?')
+            output.push(data[i]);
+        }
+    }
+    return output;
 }
 
