f599250d34154e2cd4a32fefd698536ef97de9b7
Brock <brockbvp@gmail.com>
2023-02-08 08:06:30 -0800

    adding new files

---
+++ packpoints/src/components/Home.js
@@ -0,0 +1,98 @@
+import { sortByHall, sheetProcessing } from '.././database';
+import React, { useState, useEffect } from 'react';
+import Select from 'react-select'
+
+import { Leaderboard } from './Leaderboard';
+
+import './Home.css'
+
+
+// const url = 'https://script.google.com/macros/s/AKfycbx3EYsAmGUp3nql4gkRvcuGOTSaVL1ZO3UuNvTioxskYtY3UCcOML-_v_0mfLVJSlQ8/exec';
+
+
+
+const Home = () => {
+
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [users, setUsers] = useState([]);
+  const [allUsers, setAllUsers] = useState([]);
+  const [hall, setHall] = useState("");
+
+  const options = [
+    { value: 'Argenta Hall', label: 'Argenta' },
+    { value: 'Canada Hall', label: 'Canada' },
+    { value: 'Great Basin Hall', label: 'Great Basin' },
+    { value: 'Manzanita/Juniper Hall', label: 'Manzanita/Juniper' },
+    { value: 'Nevada LLC', label: 'Nevada LLC' },
+    { value: 'Nye Hall', label: 'Nye' },
+    { value: 'Peavine Hall', label: 'Peavine' },
+    { value: 'Sierra Hall', label: 'Sierra' },
+    { value: 'All Halls', label: 'All Halls' }
+  ]
+
+  const handleHallChange = (selectedOption) => {
+    setHall(selectedOption);
+    console.log(`Option selected:`, selectedOption);
+    setUsers(sortByHall(allUsers, selectedOption));
+  }
+  
+  // useEffect(() => {
+  //   var requestOptions = {
+  //     method: 'GET',
+  //     redirect: 'follow'
+  //   };  
+  //   fetch(url, requestOptions)
+  //     .then(res => res.json())
+  //     .then(
+  //       (result) => {
+  //         setIsLoaded(true);
+  //         setUsers(dataProcessing(result));
+  //         setAllUsers(dataProcessing(result));
+  //         setUsers(sheetProcessing());
+  //         setAllUsers(sheetProcessing)
+  //       },
+  //       // Note: it's important to handle errors here
+  //       // instead of a catch() block so that we don't swallow
+  //       // exceptions from actual bugs in components.
+  //       (error) => {
+  //         setIsLoaded(true);
+  //         setError(error);
+  //       }
+  //     )
+  // }, [])
+
+  useEffect(() => {
+    fetch('https://cdn.jsdelivr.net/gh/Academic-Mentors/packpoints/packpoints/id_data.txt')
+      .then(response => response.text())
+      .then((text) => {
+        setIsLoaded(true)
+        setUsers(sheetProcessing(text))
+        setAllUsers(sheetProcessing(text))
+      },
+      (error) => {
+        setIsLoaded(true);
+        setError(error);
+      })
+    }, [])
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  } else if (!isLoaded) {
+    return <div className='Home'>Loading...</div>;
+  } else {
+    return (
+      <div className='Home'>
+        <h1 className='header'>Welcome to Pack Points</h1>
+        <div className='select'>
+          <Select options={options} onChange={handleHallChange}/>
+        </div>
+        <Leaderboard users={users}></Leaderboard>
+        {/* {(hall !== "") && <h1 className='hall-selector'>You have selected {hall.value}</h1>} */}
+      </div>
+    );
+  }
+  
+}
+
+export default Home;
