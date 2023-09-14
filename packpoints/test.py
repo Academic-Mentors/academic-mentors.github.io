cb5dea646c1e4357dd01efb791253015bd707138
unracademicmentors <clipon@Charlies-Air.rd.unr.edu>
2023-09-13 17:16:10 -0700

    adding the good good

--- packpoints/test.py
+++ packpoints/test.py
@@ -5,8 +5,3 @@ cred = credentials.Certificate('secret.json')
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
-doc_ref = db.collection('user_emails').document()
-doc_ref.set({
-    'name': 'John Doe',
-    'email': 'johndoe@example.com'
-})
